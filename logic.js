b32b628a92be33f541f1d491063f47bfab27b991
idoadler <github@ido.wtf>
2021-02-20 13:14:32 +0200

    add example levels

--- logic.js
+++ logic.js
@@ -1,4 +1,5 @@
 var canvas = document.querySelector("canvas");
+var cmbExample = document.getElementById("chosenExample");
 var tilesetContainer = document.querySelector(".tileset-container");
 var tilesetSelection = document.querySelector(".tileset-container_selection");
 var tilesetImage = document.querySelector("#tileset-source");
@@ -82,8 +83,19 @@ function exportImage() {
 }
 
 //Reset state to empty
-function clearCanvas() {
-    levelData.tiles = [{}, {}, {}];
+function setCanvas() {
+    if (cmbExample.value == "none") {
+        levelData.tiles = [{}, {}, {}];
+    } else if  (cmbExample.value == "exm1") {
+        levelData.tiles = [{"7-10":[0,1],"7-9":[2,1],"7-8":[2,1],"7-7":[2,1],"7-6":[2,1],"7-5":[2,3],"6-5":[1,3],"5-5":[1,3],"4-5":[0,2],"4-6":[2,4],"4-8":[1,2],"5-8":[2,2],"5-7":[1,1],"4-7":[2,1],"3-6":[1,3],"2-6":[2,5],"1-6":[1,2],"2-5":[2,3],"1-5":[0,2],"2-7":[2,1],"2-8":[2,1],"2-9":[2,1],"2-10":[2,1],"2-11":[2,2],"1-11":[1,0]},{},{}];
+    } else if  (cmbExample.value == "exm2") {
+        levelData.tiles = [{"7-10":[0,1],"7-9":[2,1],"7-8":[2,1],"7-7":[2,1],"7-6":[0,3],"7-5":[2,3],"6-5":[0,4],"5-5":[1,3],"4-5":[0,2],"4-6":[2,2],"3-6":[1,3],"2-6":[2,5],"1-6":[1,2],"2-5":[2,3],"1-5":[0,2],"2-7":[2,1],"2-8":[2,1],"2-9":[2,1],"2-10":[2,1],"2-11":[2,2],"1-11":[1,0],"6-2":[1,1],"6-3":[2,1],"6-4":[2,1],"11-6":[2,0],"10-6":[1,3],"9-6":[1,3],"8-6":[1,3]},{},{}];
+    } else if  (cmbExample.value == "exm3") {
+        levelData.tiles = [{"13-8":[2,2],"13-7":[2,1],"13-6":[2,1],"13-5":[1,1],"12-8":[1,3],"11-8":[0,4],"10-8":[1,0],"11-7":[2,1],"11-6":[2,1],"11-5":[1,1],"4-5":[1,3],"5-5":[1,4],"12-10":[2,5],"1-8":[0,1],"5-8":[0,1],"8-6":[0,1],"1-3":[2,5],"1-5":[1,1],"8-5":[1,1],"3-5":[1,0],"1-6":[2,1],"1-7":[2,1],"5-6":[2,1],"5-7":[2,1],"6-5":[2,0]},{},{}];
+    }
+    else{
+        print(cmbExample.value);
+    }
     draw();
 }
 
@@ -96,7 +108,7 @@ function setLayer(newLayer) {
     if (oldActiveLayer) {
         oldActiveLayer.classList.remove("active");
     }
-    document.querySelector(`[tile-layer="${currentLayer}"]`).classList.add("active");
+    // document.querySelector(`[tile-layer="${currentLayer}"]`).classList.add("active");
 }
 
 function draw() {
